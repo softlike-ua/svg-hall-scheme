c481d645d55acd154c7e4e80fc08e550f8d1a23c
Yurii Tkachuk <juuutkachuk@gmail.com>
2023-07-05 15:58:41 +0300

    - add env file

--- app.js
+++ app.js
@@ -2,22 +2,26 @@ const express = require('express');
 const mongoose = require("mongoose");
 const cors = require("cors")
 const path = require("node:path");
+require('dotenv').config()
+
 const ImgRouter = require('./src/img/ImgRouter')
 
 const app = express();
 
 app.use(express.json({limit: '1mb'}));
 app.use(express.urlencoded({extended: false, limit: '500kb'}));
-
-app.use(cors({origin: ['http://localhost:4173','http://localhost:5173'], credentials: true}));
+app.use(cors({
+    origin: [process.env.CLIENT_HOST_DEV, process.env.CLIENT_HOST_PREVIEW],
+    credentials: true
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/img', ImgRouter)
 
 const start = async () => {
-    const PORT = 5000
+    const PORT = process.env.PORT
     try {
-        await mongoose.connect('mongodb://localhost:27017/svg_s')
+        await mongoose.connect(process.env.MONGO_URI)
         app.listen(PORT, () => console.log(`SERVER STARTED ON PORT ${PORT} !!!!`))
 
     } catch (e) {
